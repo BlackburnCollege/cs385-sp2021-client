36ed4070fff9781a0b8b8bee05311d79513aa96d
Rnghino <kerleyramsey@gmail.com>
2021-02-08 22:17:48 -0600

    Main menu

---
+++ Assets/Scripts/Token.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Token : MonoBehaviour
+{
+    public Text token;
+    string findToken;
+    
+    private void Start()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                findToken = ip.ToString();
+            }
+        }
+       
+        SetToken(findToken);
+    }
+
+
+
+
+    public void SetToken(string insertedToken){
+        token.text= "IP: " + insertedToken;
+
+        }
+}
