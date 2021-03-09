522971681950d259be5bd5c0a526c0700423b8ca
MikeTheAnomaly <methoff@gmail.com>
2021-03-09 06:00:22 -0500

    lol helped lane left code

---
+++ Assets/Roatate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roatate : MonoBehaviour
+{
+    //this code was to help lane lol
+    public Transform target;
+    public float dist = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(transform.position, target.position) > dist)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, 1 * Time.deltaTime);
+        }
+        else
+        {
+
+            this.transform.RotateAround(target.position, Vector3.up, 20f * Time.deltaTime);
+        }
+    }
+}
