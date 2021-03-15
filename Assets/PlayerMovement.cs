d665aaf19b993a3515fc96918509921b26645715
MikeTheAnomaly <methoff@gmail.com>
2021-03-15 19:06:47 -0400

    yyertt

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Controllable controller;
+    public Animator playerAnimation;
 
     public float maxSpeed = 10; //m/s
     public float acceleration = 5; //m/s^2
@@ -23,15 +24,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float curSpeed = Vector3.Magnitude(new Vector3(rb.velocity.x, 0, rb.velocity.z));
+        playerAnimation.SetFloat("speed", curSpeed);
         float facing = Camera.main.transform.eulerAngles.y;
         Vector3 myTurnedInputs = Quaternion.Euler(0, facing, 0) * new Vector3(controller.Joystick1().x, 0, controller.Joystick1().y);
 
-        if (Vector3.Magnitude(new Vector3(rb.velocity.x, 0, rb.velocity.z)) < maxSpeed)
+        if (curSpeed < maxSpeed)
         {
             Vector3 movment = (new Vector3(myTurnedInputs.x * acceleration * Time.deltaTime, 0, myTurnedInputs.z * acceleration * Time.deltaTime));
-
-            rb.velocity = rb.velocity + movment + ((myTurnedInputs - rb.velocity.normalized) * (deceleration * Time.deltaTime));
+            Vector3 xzVector = rb.velocity;
+            xzVector.y = 0;
+            rb.velocity = rb.velocity + movment + ((myTurnedInputs - xzVector.normalized) * (deceleration * Time.deltaTime));
             
         }
         //rb.AddForce(new Vector3(controller.Joystick1().x * acceleration * Time.deltaTime, 0, controller.Joystick1().y * acceleration * Time.deltaTime));
