1581fe0038c32f194a377dc0d75a850d84c9d1ef
Rnghino <kerleyramsey@gmail.com>
2021-03-18 22:15:17 -0500

    game master

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,24 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    //goToSettings changes the scene from what ever scene you are on to the settings scene
     public void goToSettings()
     {
         Debug.Log("ran");
         SceneManager.LoadScene("Settings");
     }
 
+    //goToPlay changes the scene from what ever scene you are on to the play scene
     public void goToPlay()
     {
         Debug.Log("ran");
         SceneManager.LoadScene("Play");
     }
 
+    //goToMainMenu changes the scene from what ever scene you are on to the MainMenu scene
     public void goToMainMenu()
     {
         Debug.Log("ran");
         SceneManager.LoadScene("MainMenu");
     }
 
+    //goToExit leaves the game when you click the exit button
     public void goToExit()
 
     {
