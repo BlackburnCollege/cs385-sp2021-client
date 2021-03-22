96ab512e3d9300f0bae54f1cb96e3cf3af45d028
Rnghino <kerleyramsey@gmail.com>
2021-03-21 21:58:15 -0500

    reeeeeeeeeeee

--- Assets/Scripts/Puzzles.cs
+++ Assets/Scripts/Puzzles.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : MonoBehaviour
+public class Puzzles : MonoBehaviour
 {
-    bool puzzleComplete;
-
-    
     // Start is called before the first frame update
     void Start()
     {
