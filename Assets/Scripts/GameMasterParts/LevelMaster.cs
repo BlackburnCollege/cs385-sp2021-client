870b4e2fe59fbc2b28a237b62d596c7948f702aa
Rnghino <kerleyramsey@gmail.com>
2021-03-22 20:32:44 -0500

    ghgthfghgf

--- Assets/Scripts/GameMasterParts/LevelMaster.cs
+++ Assets/Scripts/GameMasterParts/LevelMaster.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Puzzles : MonoBehaviour
+public class LevelMaster : MonoBehaviour
 {
+    public List<Puzzles> puzzles = new List<Puzzles>();
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/GameMasterParts/PressurePlatePuzzle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PressurePlatePuzzle : MonoBehaviour, Puzzles
+{
+    public bool PuzzleCompleted {get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private List<Player> players = new List<Player>();
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (!players.Contains(collision.gameObject.GetComponent<Player>()))
+            {
+                players.Add(collision.gameObject.GetComponent<Player>());
+                if (GameplayGM.)
+                {
+
+                }
+            }
+        }
+    }
+
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (players.Contains(collision.gameObject.GetComponent<Player>()))
+            {
+                players.Remove(collision.gameObject.GetComponent<Player>());
+            }
+        }
+    }
+}
