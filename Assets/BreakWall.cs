9dba1d2896066a48e5b1b00a506a7fae320c7b93
MaxSchaad <kenneth.schaad@blackburn.edu>
2021-03-30 01:25:56 -0500

    Level1Update
    Level1Update

---
+++ Assets/BreakWall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakWall : Enemy
+{
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health < 0)
+        {
+            Object.Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
