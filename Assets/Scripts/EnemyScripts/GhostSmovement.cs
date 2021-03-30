d0c30aa59427c1ea42cc556e13c30cf7d15083b3
MaxSchaad <kenneth.schaad@blackburn.edu>
2021-03-29 20:43:03 -0500

    Ghost Update
    Ghost Update

--- Assets/Scripts/EnemyScripts/GhostSmovement.cs
+++ Assets/Scripts/EnemyScripts/GhostSmovement.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class GhostSmovement : MonoBehaviour
+public class GhostSmovement : MonoBehaviour, Characterable
 {
 
     private float speed = 2.0f;
+    
+    public float lookRadius = 10f;
+    
+    public float distanceFromTarget = 3f;
+    
     private GameObject player;
+    
     private GameObject[] playerList;
+    
     private Vector3 playerPos;
+
+    private Rigidbody rb;
+
+    public string Name { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public float Health { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public float MaxSpeed { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public float Acceleration { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public Weaponable weapon { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
 
     // Start is called before the first frame update
     void Start()
     {
         player = getNearestPlayer();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,10 +40,39 @@ public class GhostSmovement : MonoBehaviour
         player = getNearestPlayer();
 
 
-
         playerPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+        
+        float distanceFromPlayer = Vector3.Distance(transform.position, playerPos);
+
+        Debug.Log(distanceFromPlayer);
+
+        if (distanceFromPlayer <= lookRadius)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+
+            if (distanceFromPlayer < 1.5f)
+            {
+                Attack();
+            }
+            
+
+        }
+
+        faceTarget();
+
+       
     }
+
+
+    void faceTarget()
+    {
+        Vector3 direction = (playerPos - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
+    }
+
 
     GameObject getNearestPlayer()
     {
@@ -64,5 +110,15 @@ public class GhostSmovement : MonoBehaviour
     }
 
 
+
+    public void Attack()
+    {
+        GetComponentInChildren<Animator>().SetBool("Attack", true);
+    }
+
+    public void Movement()
+    {
+        throw new System.NotImplementedException();
+    }
 }
 
